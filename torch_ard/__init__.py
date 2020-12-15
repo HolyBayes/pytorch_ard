58c18e155d05188a32858abc598d721f75748544
Artem Ryzhikov <artemryzhikov@gmail.com>
2020-12-15 10:59:23 +0300

    Release 0.2.4

--- torch_ard/__init__.py
+++ torch_ard/__init__.py
@@ -1,5 +1,5 @@
 _author__ = 'Artem Ryzhikov'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 __all__ = ['LinearARD', 'Conv2dARD', 'get_ard_reg', 'get_dropped_params_ratio', 'ELBOLoss']
 
 from .torch_ard import LinearARD, Conv2dARD, get_ard_reg, get_dropped_params_ratio, ELBOLoss
